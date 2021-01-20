1235fbb0a83d008839f3291a52599675cf5c66bc
Gus Bikos <gusbikos@gmail.com>
2021-01-20 23:26:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,11 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array |
     #binding.pry
     value.each do |inner_key, names|
-      binding.pry
+      #binding.pry
+      names.each do |name|
+        binding.pry
+
+      end
     end
   end
 binding.pry
